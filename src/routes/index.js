e57a6618fd4efef9e62e3b74cbb188b12e8b829a
Mark Ho <54167226+MarkHo-0@users.noreply.github.com>
2022-11-27 12:26:16 +0000

    Update function name from getConnection to getDB

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,13 @@ import { Router } from "express";
 const router = Router()
 
 import { mountDB } from '../middlewares/mountDB.js'
-import { getConnection } from '../database/index.js'
+import { getDB } from '../database/index.js'
 import { getHomeData } from '../controllers/home.js'
 
 const ThreadRoutes = require('./thread.js')
 
 //掛載資料庫入口到每個請求上
-router.use(mountDB(getConnection))
+router.use(mountDB(getDB))
 
 //路由分發
 router.get('/home', getHomeData)
