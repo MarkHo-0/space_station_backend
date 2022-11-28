a93e8d30cba6d345738ab5d4146bffe8d074fe5d
Mark Ho <54167226+MarkHo-0@users.noreply.github.com>
2022-11-28 14:45:10 +0000

    Simplify getThreads function

--- src/controllers/thread.js
+++ src/controllers/thread.js
@@ -1,4 +1,4 @@
-import {RouteFunction} from '../types/express.js'
+import { RouteFunction } from '../types/express.js'
 import { Thread } from '../models/thread.js';
 import { Comment } from '../models/comment.js';
 
@@ -10,23 +10,16 @@ export function getThreads(req, res) {
     const {cursor, order, pid, fid} = req.params
 
     //參數校驗
-    if (typeof cursor != 'string' || cursor < 1) cursor = null
+    if (typeof cursor != 'string' || cursor < 1) cursor = 0
     if (typeof order != 'number' || order < 0 || order > 1) order = 0
     if (typeof pid != 'number' || pid < 0 || pid > 1) pid = 0
     if (typeof fid != 'number' || fid < 0) fid = 0
 
-    /** @type {Promise<Thread[]>} */
-    let getFunction;
-
     //是按熱度還是按發文時間先後搜尋
-    if (order == 0) {
-        getFunction = req.db.thread.getHeatestWithParams(pid, fid, MAX_THREADS_PRE_GET, cursor)
-    } else {
-        getFunction = req.db.thread.getNewestWithParams(pid, fid, MAX_THREADS_PRE_GET, cursor)
-    }
+    const getFunction = order == 0 ? req.db.thread.getHeatestWithParams : req.db.thread.getNewestWithParams 
 
     //執行資料庫查詢
-    getFunction.then(threads => {
+    getFunction(pid, fid, MAX_THREADS_PRE_GET, cursor).then(threads => {
         //將資料轉為 JSON 並發送
         threads = threads.map( t => t.toJSON() )
         res.send(threads)
