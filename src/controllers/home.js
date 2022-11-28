2a8317423a7a88d1827366895ff6a416f3df4cb6
Mark Ho <54167226+MarkHo-0@users.noreply.github.com>
2022-11-28 14:45:45 +0000

    Fix error in home controller with wrong function

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -8,13 +8,13 @@ export function getHomeData(req, res) {
     const getSchoolNewsFunc = req.db.news.getAll()
     Promise.all([getThreadsFunc, getSchoolNewsFunc]).then(data => {
         const homeData = {
-            threads: data[0],
+            threads: data[0].map( t => t.toJSON()),
             news: data[1],
         }
     
         //若果用戶為登入，則返回用戶名和用戶編號
         if (req.user) {
-            homeData['user'] = req.user.toNameTag()
+            homeData['user'] = req.user.toJSON()
         }
         
         res.send(homeData)
