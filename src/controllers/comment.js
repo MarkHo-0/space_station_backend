a980fc597427c0e525cd1034f3847bf8ba05695e
PIANISTSUSF <114916487+PIANISTSUSF@users.noreply.github.com>
2022-12-05 07:28:05 +0000

    modiffied comment's function

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -4,22 +4,32 @@ import { Comment } from '../models/comment.js';
 
 /** @type {RouteFunction} */
 export function postComment(req, res) {
+    
 
+    
 }
 
 /** @type {RouteFunction} */
 export function getComment(req, res) {
-
+    
 }
 
 /** @type {RouteFunction} */
 export function reactComment(req, res) {
+    let cid = parseInt(req.path['cid'])
+    let cr_type = parseInt (req.parms['type'])
+
+    if ( isNaN(cid) ) 
+        return res.status(400).send()
+
+    if ( isNaN(cr_type) || cr_type !== 0 || cr_type !== 1)
+        return res.status(400).send()
 
 }
 
 /** @type {RouteFunction} */
 export function pinOrUnpinComment(req, res) {
-
+    
 }
 
 /** @type {RouteFunction} */
--- src/database/comment.js
+++ src/database/comment.js
@@ -1,5 +1,5 @@
 import { Pool } from 'mysql2'
-import { commentFromDB} from '../models/comment.js'
+import { commentFromDB} from '/models/commentjs'
 
 export class Comment{
 
@@ -7,27 +7,57 @@ export class Comment{
   db = null
 
   constructor(connection) {
-    this.db = connection
+    db = connection
   }
 
   async getOne(cid){
+    const [_, comment] = await this.db.promise().execute(`--sql
+    SELECT 
+    comment_id, content, sender, create_time, 
+    like_count ,dislike_count, reply_to, my_reation
+    WHERE 
+    cid == ?
+    `[cid])
+
     return commentFromDB()
   }
   
   async getMany(cid_array){
-    const comments_raw = []
-    return comments_raw.map( c => commentFromDB(c))
+    const arr_str = cid_array.join(" ");
+    const [_, comment] = await this.db.promise().execute(`--sql
+    SELECT * FROM comment WHERE cid IN [?]
+    const comments_raw = []`)
+    return comments_rawmap( c => commentFromDB(c))
   }
 
   async createReaction(cid, type_id, user_id) {
+    const [_, comment_reaction] =await this.db.promise().execute(`--sql
+    INSERT into comment_reaction 
+    ( 'cid', uid, type)
+    `,[cid, type_id, user_id])
+
     return true
   }
 
   async updateReaction(cid, type_id, user_id) {
+    const [_, comment_reaction] = await this.db.promise().execute(`--sql
+    UPDATE comment 
+    SET like_count = like_count + 1
+    SET dislike_count = dislike_count + 1
+
+    `,[type_id, user_id])
+    
     return true
+    
   }
 
   async removeReaction(cid, user_id) {
+    const [_, comment_reaction] = await this.db.promise().execute(`--sql
+    UPDATE comment 
+    SET like_count = like_count - 1
+    SET dislike_count = dislike_count - 1
+
+    `,[type_id, user_id])
     return true
   }
   
