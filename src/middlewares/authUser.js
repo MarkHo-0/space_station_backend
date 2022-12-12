49b31e91a8710d370a2e91dfcab62218b678881b
Mark Ho <s12059gsps@gmail.com>
2022-12-12 18:49:01 +0800

    Update auth user middware to new db interface

--- src/middlewares/authUser.js
+++ src/middlewares/authUser.js
@@ -4,51 +4,47 @@
 
 /** @type {RouteFunction} */
 export async function authUser(req, res, next) {
-    const token = req.header['Authorization']
+  //若無附帶令牌則返回 401 錯誤
+  const token = req.header['Authorization']
+  if (!token) return res.status(401).send()
 
-    //若無附帶令牌則返回 401 錯誤
-    if (!token) {
-      return res.status(401).send()
-    }
+  //解密令牌
+  const token_info = await req.db.user.decryptToken(token)
 
-    //解密令牌
-    const token_info = await req.db.user.decryptToken(token)
+  //如無法獲取則表示令牌無效
+  if (!token_info) {
+    return res.status(401).send()
+  }
 
-    //如無法獲取則表示令牌無效
-    if (!token_info) {
-      return res.status(401).send()
-    }
+  //如令牌過期則刪除並且返回錯誤
+  if (token_info.expired) {
+    req.db.user.removeLoginToken(token)
+    return res.status(401).send()
+  }
 
-    //如令牌過期則刪除並且返回錯誤
-    if (token_info.expired) {
-      req.db.user.removeLoginToken(token)
-      return res.status(401).send()
-    }
-    
-    //透過用戶編號獲取用戶資料
-    req.db.user.getOne(token_info.user_id).then(user => {
-      //將資料掛載在 req 對象上，讓後方路由可獲取使用
-      req.user = user
-      next()
-    }).catch( res.status(401).send() )
+  //獲取用戶資料
+  const user = await req.db.user.getOneByUID(token_info.user_id)
+  if (!user) return res.status(401).send()
+  
+  //掛載用戶資料在 req 對象上
+  req.user = user
+  next()
 }
 
 /** @type {RouteFunction} */
 export async function tryAuthUser(req, _, next) {
-    const token = req.header['Authorization']
+  //若無附帶令牌則直接略過身份驗證
+  const token = req.header['Authorization']
+  if (!token) return next()
 
-    //若無附帶令牌則直接略過身份驗證
-    if (!token) return next()
+  //解密令牌
+  const token_info = await req.db.user.decryptToken(token)
 
-    //解密令牌
-    const token_info = await req.db.user.decryptToken(token)
+  //若令牌錯誤或過期也略過驗證
+  if (!token_info || token_info.expired) return next()
 
-    //若令牌錯誤或過期也略過驗證
-    if (!token_info || token_info.expired) return next()
-
-    //無論是否獲取到用戶資料，都繼續前往下一個路由
-    req.db.user.getOne(token_info.user_id)
-        .then( user => req.user = user)
-        .catch(null)
-        .finally( () => next() )
+  //嘗試獲取用戶資料並掛載在 req 對象上
+  const user = await req.db.user.getOneByUID(token_info.user_id)
+  if (user) req.user = user
+  next()
 }
