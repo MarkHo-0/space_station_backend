6725b44678ac47d6b1a317bd399f41f309ca6bcc
Flenix Lee <fibe.flenixprivate.com>
2023-01-10 00:54:56 +0800

    add new function class_Swap

---
+++ src/toolbox/class_Swap.js
@@ -0,0 +1,11 @@
+import { UserFromDB } from "../models/user";
+import { User } from "../models/user";
+ 
+
+export async function class_Swap(course_Code, clNum) {
+    
+    if (course_Code!=null || clNum != null) 
+     return res.status(404).send();
+}
+
+
