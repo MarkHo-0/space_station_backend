f4083baee607a8522a1af03bb1c267b34d1628f9
Flenix Lee <fibe.flenixprivate.com>
2023-01-10 01:05:23 +0800

    modified class_Swap

--- src/toolbox/class_Swap.js
+++ src/toolbox/class_Swap.js
@@ -1,10 +1,12 @@
 import { UserFromDB } from "../models/user";
 import { User } from "../models/user";
- 
+
+var regexConst = new RegExp('[A-Za-z]{4}[0-9]{4}');
+
 
 export async function class_Swap(course_Code, clNum) {
     
-    if (course_Code!=null || clNum != null) 
+    if (course_Code!=null || clNum != null || course_Code!=regexConst) 
      return res.status(404).send();
 }
 
