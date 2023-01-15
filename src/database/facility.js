acf44d4fe975bf929f83bdf7f86c9443b2f9ae14
Flenix Lee <fibe.flenixprivate.com>
2023-01-15 23:30:20 +0800

    modified facility.js

--- src/database/facility.js
+++ src/database/facility.js
@@ -1,5 +1,17 @@
 import { createPool } from 'mysql2/promise'
 
 export class Facility {
-    
+
+
+    constructor(connection) {
+        this.db = connection
+      }
+
+    async getclnum ()
+      
+
+
+   
+
+       
 }
