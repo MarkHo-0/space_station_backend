008cf83b736427cd8743215401372bcde2d9156c
Mark Ho <54167226+MarkHo-0@users.noreply.github.com>
2023-01-27 07:34:09 +0000

    Add routes of Class Swap

--- src/controllers/class_swap.js
+++ src/controllers/class_swap.js
@@ -1,9 +1,47 @@
-import { validateCourseCode } from "../utils/dataValidation";
+import { validateContactInfo, validateCourseCode, validateInteger } from "../utils/dataValidation";
+
+/** @typedef {import('../types/express.js').RouteFunction} RouteFunction */
 
 /** @type {RouteFunction} */
-export async function CLSwapWrongInput(course_Code, clNum) {
-    const code = validateCourseCode
-    if (!code) return res.status(422).send('please input in correct course code in format (CCEN 4003) or class number in format (CL01)')
+export function getStatus(req, res) {
+}
+
+/** @type {RouteFunction} */
+export async function searchSwapRequest(req, res) {
+}
+
+/** @type {RouteFunction} */
+export function viewSwapRequests(req, res) {
+
+}
+
+/** @type {RouteFunction} */
+export async function postSwapRequest(req, res) {
+  //校驗科目代號
+  const code = validateCourseCode(req.body['course_code'])
+  const course = await req.db.school.getCourse(code)
+  if (!course) return res.status(422).send('Invalid Course Code')
+
+  //校驗班級編號
+  const current_class = req.body['current_class_num']
+  const expected_class = req.body['expected_class_num']
+  if (!course.isClassValid(current_class) || !course.isClassValid(expected_class)) {
+    return res.status(422).send('Invalid Class Number')
+  }
+
+  //校驗聯絡資料
+  const contact = validateContactInfo(req.body)
+  if (!contact) return res.status(422).send('Invalid Contact Info')
+}
+
+/** @type {RouteFunction} */
+export function removeSwapRequest(req, res) {
+
+}
+
+/** @type {RouteFunction} */
+export function performSwap(req, res) {
+
 }
 
 
--- src/routes/class_swap.js
+++ src/routes/class_swap.js
@@ -1,8 +1,30 @@
 import { Router } from "express";
-import * as Controller from "..controllers/class_Swap.js"
-import { authUser } from "../middlewares/authUser";
+import * as Controller from "../controllers/class_swap.js"
+import { authUser, tryAuthUser } from "../middlewares/authUser";
 
 const router = Router()
 
-router.get('/', authUser, Controller.CLSwapWrongInput);
+//獲取組件狀態 (如果有身分訊息則會返回交換概覽)
+router.get('/', tryAuthUser, Controller.getStatus);
 
+
+//進行統一身分驗證 (下方路由均需要有身分訊息)
+router.use(authUser)
+
+
+//搜尋他人發布的交換請求
+router.get('/search', Controller.searchSwapRequest)
+
+//檢視自己的交換請求
+router.get('/request', Controller.viewSwapRequests)
+
+//發布新的交換請求
+router.post('/request', Controller.postSwapRequest)
+
+//刪除過往的交換請求
+router.delete('/request/:id', Controller.removeSwapRequest)
+
+//進行交換
+router.post('/swap', Controller.performSwap)
+
+export default router
