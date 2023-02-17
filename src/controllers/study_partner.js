5facb9068c7e8bb8ec7d1f336b59cc4d7d0e565a
Flenix Lee <fibe.flenixprivate.com>
2023-02-18 01:37:59 +0800

    import typefunction and valid function UITLs

---
+++ src/controllers/study_partner.js
@@ -0,0 +1,4 @@
+import { validateContactInfo, validateCourseCode, validatePositiveInt } from "../utils/dataValidation.js";
+/** @typedef {import('../types/express.js').RouteFunction} RouteFunction */
+
+
