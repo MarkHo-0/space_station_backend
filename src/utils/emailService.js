4b962e521d4598df47b168bcc15f86acf7b6579e
Mark Ho <s12059gsps@gmail.com>
2023-03-11 17:57:13 +0800

    Beautify verification email

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -18,13 +18,31 @@ export async function connectGmail() {
   })
 }
 
-export async function sendVfEmail(sid, vf_code) {
+export async function sendVfEmail(sid = 0, vf_code = 0) {
   if ( !gmail_server ) throw new Error('Email Server Not Found')
 
   const email = await gmail_server.sendMail({
-    from: process.env['EMAIL_USER'],
+    from: `Space Station<${process.env['EMAIL_USER']}>`,
     to: sid + "@learner.hkuspace.hku.hk",
     subject: "Verify your Student ID",
-    text: "This is the verification code: " + vf_code
+    html: temple.replace('{sid}', sid.toString()).replace('{code}', vf_code.toString())
   })
 }
+
+const temple = 
+`<div style="font-family: 'Source Sans Pro', sans-serif; margin: 0;">
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;900&display=swap');
+  </style>
+  <div style="background-color: #7c8ec1; width: 100%;">
+    <div style="max-width: 600px; margin: 0 auto; padding:15px;">
+      <h1 style="color: white; letter-spacing: 2px; text-align: center;">SPACE STATION</h2>
+    </div>
+  </div>
+  <div style="max-width: 600px; height: 500px; margin: 0 auto; padding: 30px 15px; text-align: justify;">
+    <span style="font-size: 20px; margin-bottom: 5px; display: block;">Hi {sid},</span>
+    Thank you for registering with Space Station, here is your verification code.
+    Please enter this code to verify your identity and sign in.
+    <h2 style="text-align: center; font-size: 40px; letter-spacing: 10px;">{code}</h2>
+  </div>
+</div>`
